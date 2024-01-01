f6d48cd4ae8964abd405bee819b9871c65b9d5bb
mallexibra <mallexibra@gmail.com>
2024-01-01 10:46:19 +0700

    feat: connect ui to api

--- lib/pages/ReviewPage.dart
+++ lib/pages/ReviewPage.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:rent_mobileapps/components/BarNavigation.dart';
 import 'package:rent_mobileapps/components/Layout.dart';
+import 'package:rent_mobileapps/sevices/ReviewServices.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ReviewPage extends StatefulWidget {
   const ReviewPage({super.key});
@@ -10,6 +14,25 @@ class ReviewPage extends StatefulWidget {
 }
 
 class _ReviewPageState extends State<ReviewPage> {
+  var reviews = <dynamic>[];
+
+  getReviews() async {
+    await ReviewServices().getReviews();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final data = jsonDecode(prefs.getString('reviews')!) as List<dynamic>;
+
+    reviews = data;
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getReviews();
+  }
+
   @override
   Widget build(BuildContext context) {
     double c_width = MediaQuery.of(context).size.width * 0.75;
@@ -29,63 +52,65 @@ class _ReviewPageState extends State<ReviewPage> {
             height: 12,
           ),
           Expanded(
-              child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(66, 205, 205, 205),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 42,
-                        height: 42,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                            "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?q=80&w=1448&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+              child: ListView.builder(
+                  itemCount: reviews.length,
+                  itemBuilder: (_, index) {
+                    return Container(
+                      width: double.maxFinite,
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(bottom: 12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color.fromARGB(66, 205, 205, 205),
                       ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Column(
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "User Keren",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Row(
-                            children: [
-                              Text("Rating: "),
-                              Icon(Icons.star_rate_rounded,
-                                  color: Colors.amber, size: 18),
-                              Text("4")
-                            ],
-                          ),
                           Container(
-                            width: c_width,
-                            child: Text(
-                              "Wah studionya bagus banget dan bersih juga",
-                            ),
+                            width: 42,
+                            height: 42,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child:
+                                    reviews[index]['users']['profile'] != null
+                                        ? Image.network(
+                                            reviews[index]['users']['profile'],
+                                            fit: BoxFit.cover,
+                                          )
+                                        : Image.asset(
+                                            'images/user.png',
+                                            fit: BoxFit.cover,
+                                          )),
                           ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                reviews[index]['users']['name'],
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  Text("Rating: "),
+                                  Icon(Icons.star_rate_rounded,
+                                      color: Colors.amber, size: 18),
+                                  Text("${reviews[index]['rating']}")
+                                ],
+                              ),
+                              Container(
+                                width: c_width,
+                                child: Text(reviews[index]['deskripsi']),
+                              ),
+                            ],
+                          )
                         ],
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ))
+                      ),
+                    );
+                  }))
         ],
       )),
       bottomNavigationBar: BarNavigation(
---
+++ lib/sevices/ReviewServices.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ReviewServices {
+  final dio = Dio();
+  final url = "http://localhost:8000/api";
+
+  getReviews() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('token');
+
+      var response = await dio.get("$url/reviews?id-user=1",
+          options: Options(headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer $token"
+          }));
+
+      Map obj = response.data;
+
+      if (obj['status']) {
+        final data = jsonEncode(obj['data']);
+
+        prefs.setString("reviews", data);
+
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+}
